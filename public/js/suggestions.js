0f82b2f581c3c47b2d419717d5c7e6a640813a85
Lindsay <lrroberts0122@gmail.com>
2015-04-15 23:41:41 -0400

    Suggestions Page Finished
    Finished the stylings for the Suggestions Page.
    Issue #9

--- public/js/suggestions.js
+++ public/js/suggestions.js
@@ -14,5 +14,16 @@
 		  total: 360,
 		  showLabel: false
 		});
+		
+		new Chartist.Line('.historicalData .ct-chart', {
+		  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+		  series: [
+		    [12, 9, 7, 8, 5],
+		    [2, 1, 3.5, 7, 3],
+		    [1, 3, 4, 5, 6]
+		  ]
+		}, {
+		  fullWidth: true
+		});
 	});
 })(jQuery);
